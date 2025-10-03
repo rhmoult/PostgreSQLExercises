c78ae76a530b49141ea36adad0b0e6b99e9c0815
Rich Moulton <rhmoult@gmail.com>
2025-10-03 07:59:18 -0400

    Fix capitalization

--- joinsAndSubqueries/workOutTheStartTimesOfBookingsForTennisCourts.sql
+++ joinsAndSubqueries/workOutTheStartTimesOfBookingsForTennisCourts.sql
@@ -1,9 +1,9 @@
-SELECT bks.starttime as start, fac.name as name
+SELECT bks.starttime AS start, fac.name AS name
 FROM cd.facilities fac
 	INNER JOIN cd.bookings bks
 	ON fac.facid = bks.facid
 WHERE
-	fac.name like 'Tennis Court%' AND
+	fac.name LIKE 'Tennis Court%' AND
 	bks.starttime >= '2012-09-21' AND
 	bks.starttime < '2012-09-22'
 ORDER BY bks.starttime
