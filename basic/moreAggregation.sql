cd3648ad8c434cecf54479ebaf036919a7a2c356
Rich Moulton <rhmoult@gmail.com>
2025-10-03 07:51:12 -0400

    Make solution cleaner for basic ex 12

--- basic/moreAggregation.sql
+++ basic/moreAggregation.sql
@@ -1,6 +1,3 @@
-WITH lastdate as
-(SELECT max(joindate) as maxjoin
- FROM cd.members)
  SELECT firstname, surname, joindate
  FROM cd.members
- WHERE joindate = (SELECT maxjoin from lastdate)
+ WHERE joindate = (SELECT max(joindate) from cd.members)
